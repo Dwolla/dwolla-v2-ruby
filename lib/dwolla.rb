e1b098aff474fc3e9ce90bbdce0bca3b7e8b1bcb
Stephen Ausman <stephen@dwolla.com>
2016-01-06 11:05:05 -0600

    Add Dwolla::Error

--- lib/dwolla.rb
+++ lib/dwolla.rb
@@ -5,6 +5,7 @@ require "dwolla/client"
 require "dwolla/portal"
 require "dwolla/auth"
 require "dwolla/token"
+require "dwolla/errors/error"
 
 module Dwolla
 end
---
+++ lib/dwolla/errors/error.rb
@@ -0,0 +1,4 @@
+module Dwolla
+  class Error < StandardError
+  end
+end
